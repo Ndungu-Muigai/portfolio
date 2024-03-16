700095178b2abe8f6d6f322dc663f33f63dd03ff
Samuel Muigai <ndungusam01@gmail.com>
2024-03-16 10:17:10 +0300

    Initialized the project and set up the files

---
+++ src/Pages/Home.js
@@ -0,0 +1,12 @@
+const Home = () => 
+{
+    return ( 
+        <>
+            <h1>Intro Text here</h1>
+            <img src='/' alt='Profile'/>
+        </>
+     );
+}
+ 
+export default Home;
+
